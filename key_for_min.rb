ee684f028633d695d85088739fe7c11872b0bb64
Kim Gonzales <kimgo589@gmail.com>
2018-01-27 01:16:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,9 @@ def key_for_min_value(name_hash)
     #should return array of values [x, y, z,...]
     counter = 0
     while counter < value.length
-    if value[0] < value[1]
-      value[1] = value[0] #replaces the 0 index with the larger number
-    else
-
+      if value[0] < value[1]
+        value[1] = value[0] #replaces the 0 index with the larger number
+        counter += 1
     end
+    key
 end
