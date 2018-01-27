1d098bee5a2d646014e3877e88cda1d74a0244c0
Kim Gonzales <kimgo589@gmail.com>
2018-01-27 01:15:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,13 @@
 def key_for_min_value(name_hash)
   name_hash.collect do | key, value |
     value
-    if va
-
   end
+    #should return array of values [x, y, z,...]
+    counter = 0
+    while counter < value.length
+    if value[0] < value[1]
+      value[1] = value[0] #replaces the 0 index with the larger number
+    else
+
+    end
 end
